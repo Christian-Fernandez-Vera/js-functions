a85ea3c0c5d65ad9b74736b7129ea56346c42e75
Christian Fernandez Vera <xtian.fdz@hotmail.com>
2026-08-30 00:55:46 +0200

    feat: initialize project structure with HTML template and currency converter function scaffold

---
+++ App.js/app.js
@@ -0,0 +1,9 @@
+/**
+ * 1.Stai pianificando un viaggio negli USA. Scrivi una funzione che converta una somma in Euro nel corrispondente valore in Dollari, dato il tasso di cambio del giorno.
+ * 
+ * 
+ * 
+ * 
+ */
+
+//------------------------------------------------------------
